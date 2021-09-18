ace6ccb9bc736d1aa2af4c405019426a427da92e
amrit <peaceamrit95@gmail.com>
2021-09-18 20:16:50 +0545

    first commit

---
+++ Amritwebapp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Amritwebapp.Areas.Identity.Data;
+using Amritwebapp.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Amritwebapp.Areas.Identity.IdentityHostingStartup))]
+namespace Amritwebapp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AmritwebappContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AmritwebappContextConnection")));
+
+                services.AddDefaultIdentity<AmritwebappUser>()
+                    .AddEntityFrameworkStores<AmritwebappContext>();
+            });
+        }
+    }
+}
